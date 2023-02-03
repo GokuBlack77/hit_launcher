3025360c170eb4c59f61899d4405010288d735ed
GokuBlack77 <joewesley67@gmail.com>
2023-02-03 17:31:07 +0700

    3 Februari commit

--- app/src/main/java/com/polytron/mylauncher/MainActivity.java
+++ app/src/main/java/com/polytron/mylauncher/MainActivity.java
@@ -1,5 +1,7 @@
 package com.polytron.mylauncher;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
@@ -30,10 +32,19 @@ import android.widget.Button;
 import com.polytron.mylauncher.models.CustomViewGroup;
 import com.polytron.mylauncher.screens.fragments.HomeScreenFragment;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 //    final View view = LayoutInflater.inflate(R.layout.fragment_home_screen, , false);
+// To keep track of activity's window focus
+boolean currentFocus;
+
+// To keep track of activity's foreground/background status
+boolean isPaused;
+
+Handler collapseNotificationHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,25 +65,156 @@ public class MainActivity extends AppCompatActivity {
 //                    Uri.parse(sb.toString())),123);
 //        }
 
-
         checkPermission();
 
-        try{
-            preventStatusBarExpansion(this);
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
 //        try{
-//            disablePullNotificationTouch();
+//            preventStatusBarExpansion(this);
 //        }
 //        catch (Exception ex) {
 //            ex.printStackTrace();
 //        }
 
+        try{
+            disablePullNotificationTouch();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> runningAppProcessInfo = am.getRunningAppProcesses();
+
+        for (int i = 0; i < runningAppProcessInfo.size(); i++) {
+            Log.e("Running Apps: ", runningAppProcessInfo.get(i).processName );
+//            if(runningAppProcessInfo.get(i).processName.equals("com.the.app.you.are.looking.for")) {
+//                // Do your stuff here.
+//            }
+        }
+//        checkRunningApps();
+
     }
-//
+
+    String lastAppPN = "com.polytron.mylauncher";
+    public void checkRunningApps() {
+        ActivityManager mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        String activityOnTop;
+        if (Build.VERSION.SDK_INT > 20) {
+            activityOnTop = mActivityManager.getRunningAppProcesses().get(0).processName;
+            Log.e(TAG, "checkRunningApps: " + activityOnTop);
+        } else {
+            List<ActivityManager.RunningTaskInfo> RunningTask = mActivityManager.getRunningTasks(1);
+            ActivityManager.RunningTaskInfo ar = RunningTask.get(0);
+            activityOnTop = ar.topActivity.getPackageName();
+        }
+        //Log.e("activity on TOp", "" + activityOnTop);
+
+        // Provide the packagename(s) of apps here, you want to show password activity
+        if (activityOnTop.contains("whatsapp")  // you can make this check even better
+                || activityOnTop.contains("com.polytron.mylauncher")) {
+            if (!(lastAppPN.equals(activityOnTop))) {
+                lastAppPN = activityOnTop;
+                Log.e("Whatsapp", "started");
+            }
+        } else {
+            if (lastAppPN.contains("whatsapp")) {
+                if (!(activityOnTop.equals(lastAppPN))) {
+                    Log.e("Whatsapp", "stoped");
+                    lastAppPN = "";
+                }
+            }
+            // DO nothing
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+
+        currentFocus = hasFocus;
+
+        if (!hasFocus) {
+//            //basically hide the status bar only
+//            View decorView = getWindow().getDecorView();
+//            // Hide the status bar.
+//            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+//            decorView.setSystemUiVisibility(uiOptions);
+
+            // Method that handles loss of window focus
+            collapseNow();
+
+        }
+    }
+
+    public void collapseNow() {
+
+        // Initialize 'collapseNotificationHandler'
+        if (collapseNotificationHandler == null) {
+            collapseNotificationHandler = new Handler();
+        }
+
+        // If window focus has been lost && activity is not in a paused state
+        // Its a valid check because showing of notification panel
+        // steals the focus from current activity's window, but does not
+        // 'pause' the activity
+        if (!currentFocus && !isPaused) {
+
+            // Post a Runnable with some delay - currently set to 300 ms
+            collapseNotificationHandler.postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    // Use reflection to trigger a method from 'StatusBarManager'
+
+                    Object statusBarService = getSystemService("statusbar");
+                    Class<?> statusBarManager = null;
+
+                    try {
+                        statusBarManager = Class.forName("android.app.StatusBarManager");
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                    Method collapseStatusBar = null;
+
+                    try {
+
+                        // Prior to API 17, the method to call is 'collapse()'
+                        // API 17 onwards, the method to call is `collapsePanels()`
+
+                        if (Build.VERSION.SDK_INT > 16) {
+                            collapseStatusBar = statusBarManager .getMethod("collapsePanels");
+                        } else {
+                            collapseStatusBar = statusBarManager .getMethod("collapse");
+                        }
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+
+                    collapseStatusBar.setAccessible(true);
+
+                    try {
+                        collapseStatusBar.invoke(statusBarService);
+                    } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Check if the window focus has been returned
+                    // If it hasn't been returned, post this Runnable again
+                    // Currently, the delay is 100 ms. You can change this
+                    // value to suit your needs.
+                    if (!currentFocus && !isPaused) {
+                        collapseNotificationHandler.postDelayed(this, 10L);
+                    }
+
+                }
+            }, 10L);
+        }
+    }
+
     public boolean checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!Settings.canDrawOverlays(this)) {
@@ -106,7 +248,7 @@ public class MainActivity extends AppCompatActivity {
         WindowManager manager = ((WindowManager) getApplicationContext()
                 .getSystemService(Context.WINDOW_SERVICE));
         WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
-//        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+//        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY;
         localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
 //        localLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
         localLayoutParams.gravity = Gravity.TOP;
@@ -193,6 +335,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onResume() {
         super.onResume();
+        isPaused = false;
         checkPermission();
         try{
             disablePullNotificationTouch();
@@ -200,22 +343,18 @@ public class MainActivity extends AppCompatActivity {
         catch (Exception ex) {
             ex.printStackTrace();
         }
-//        //basically hide the status bar only
-//        View decorView = getWindow().getDecorView();
-//        // Hide the status bar.
-//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-//        decorView.setSystemUiVisibility(uiOptions);
     }
 
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        isPaused = true;
 //        ActivityManager activityManager = (ActivityManager) getApplicationContext()
 //                .getSystemService(Context.ACTIVITY_SERVICE);
 //
 //        activityManager.moveTaskToFront(getTaskId(), 0);
-//    }
+    }
 
 
     private boolean loadFragment(Fragment fragment) {
